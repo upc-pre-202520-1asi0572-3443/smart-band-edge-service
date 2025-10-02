b73b6098e10fb770105d1e562a40570a3397b194
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-02 11:30:47 -0500

    feat(iam): add domain authenticate service.

---
+++ iam/domain/services.py
@@ -0,0 +1,18 @@
+from iam.domain.entities import Device
+from typing import Optional
+
+class AuthService:
+    """
+    Constructor for AuthService
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def authenticate(device: Optional[Device])->bool:
+        """
+        Authenticate a device based on its presence.
+        :param device: Device object or None
+        :return: True if the device is valid, False otherwise
+        """
+        return device is not None
